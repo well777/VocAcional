58b9ae802ab4d4b7ded70f972480d2d5d78fc788
wellington santos <50835319+well777@users.noreply.github.com>
2023-05-26 14:34:18 -0300

    Add files via upload

---
+++ Screens1.js
@@ -0,0 +1,15 @@
+
+document.getElementById('nextButton').addEventListener('click', function() {
+    window.location.href = 'https://www.example.com'; // Substitua "https://www.example.com" pelo URL desejado
+});
+function toggleTag(tag) {
+            tag.classList.toggle('selected');
+        }
+
+function toggleTag(tag) {
+            tag.classList.toggle('selected');
+        }
+
+        function redirectToPage() {
+            window.location.href = "outra_pagina.html";
+        }
